ce4cc8f5e52fe9ec6e68c275d7ca72f52e5ea2b4
mroohian <m.roohian87@gmail.com>
2021-04-19 19:49:43 +0200

    disable logs

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
+const debugMode = false;
+
+const log = debugMode ? console.log : () => {};
+
 function fib(n, prevValues = {}, level = 0) {
   const dashes = "-".repeat(level);
-  console.log(`${dashes}> [${n}]: called`);
+  log(`${dashes}> [${n}]: called`);
 
   if (prevValues[n]) {
     const prevResult = prevValues[n];
-    console.log(`${dashes}> [${n}] = ${prevResult} hit`);
+    log(`${dashes}> [${n}] = ${prevResult} hit`);
     return prevResult;
   }
 
   if (n <= 2) {
-    console.log(`${dashes}> [${n}] = 1 static`);
+    log(`${dashes}> [${n}] = 1 static`);
 
     prevValues[n] = 1;
     return 1;
   } else {
     const result =
       fib(n - 1, prevValues, level + 1) + fib(n - 2, prevValues, level + 1);
-    console.log(`${dashes}> [${n}] = ${result} calculated`);
+    log(`${dashes}> [${n}] = ${result} calculated`);
 
     prevValues[n] = result;
     return result;
